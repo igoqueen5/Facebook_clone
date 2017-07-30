66befe8ef54bed39b2c96a085484370a3af15438
igoqueen5 <igoqueen5@gmail.com>
2017-07-30 21:47:34 +0000

    メッセージ機能とフォロー機能を実装した。但しレイアウトは要修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'relationships/create'
+
+  get 'relationships/destroy'
+
   resources :topics do
     resources :comments
     post :confirm, on: :collection
@@ -10,7 +14,12 @@ Rails.application.routes.draw do
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
 }
+    resources :users, only: [:index, :show]
+    resources :relationships, only: [:create, :destroy]
 
+    resources :conversations do
+    resources :messages
+    end
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
