ffc3118cd57a2764bf1b6154b34400971fe4eb41
igoqueen5 <igoqueen5@gmail.com>
2017-07-30 14:28:54 +0000

    コメントの編集機能を実装中

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,19 +2,43 @@ class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.build(comment_params)
     @topic = @comment.topic
-    # クライアント要求に応じてフォーマットを変更
+
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to blog_path(@topic), notice: 'コメントを投稿しました。' }
+        format.html { redirect_to topic_path(@topic), notice: 'コメントを投稿しました。' }
+        format.js { render :index }
       else
         format.html { render :new }
       end
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    respond_to do |format|
+    @comment.destroy
+      format.html{redirect_to topic_path(@topic), notice: 'コメントを削除しました。'}
+      format.js{render :index}
+    end
+  end
+end
+
+  def edit
+　end
+
+
+
+  def update
+   respond_to do |format|
+    @comment.update(comment_params)
+    format.html { redirect_to topic_path(@topic), notice: 'コメントを更新しました。' }
+    format.js{ render :index }
+  end
+end
+
   private
     # ストロングパラメーター
     def comment_params
       params.require(:comment).permit(:topic_id, :content)
     end
-end
+  end
