6bb9ce9c94cce8addfc8752ec417acd8ca5b672e
igoqueen5 <igoqueen5@gmail.com>
2017-08-04 10:22:44 +0000

    Comment_controllerを修正。コメントが投稿できなかったため。但しまだeditはできない

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,44 +1,51 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:edit, :update, :destroy]
+
+
+
   def create
-    @comment = current_user.comments.build(comment_params)
-    @topic = @comment.topic
+   @comment = current_user.comments.build(comment_params)
+   @topic = @comment.topic
+     respond_to do |format|
+   @comment.save
+    format.js{ render :index }
+   end
+ end
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to topic_path(@topic), notice: 'コメントを投稿しました。' }
-        format.js { render :index }
-      else
-        format.html { render :new }
-      end
-    end
-  end
 
-  def destroy
-    @comment = Comment.find(params[:id])
-    respond_to do |format|
-    @comment.destroy
-      format.html{redirect_to topic_path(@topic), notice: 'コメントを削除しました。'}
-      format.js{render :index}
-    end
-  end
-end
 
   def edit
-　end
+   respond_to do |format|
+    format.js { render :edit }
+  end
+end
 
 
 
   def update
    respond_to do |format|
     @comment.update(comment_params)
-    format.html { redirect_to topic_path(@topic), notice: 'コメントを更新しました。' }
-    format.js{ render :index }
+   format.js{ render :index }
   end
 end
 
+
+
+  def destroy
+   respond_to do |format|
+    @comment.destroy
+     format.js{ render :index }
+   end
+ end
+
+
+
   private
-    # ストロングパラメーター
-    def comment_params
-      params.require(:comment).permit(:topic_id, :content)
-    end
-  end
+   def comment_params
+    params.require(:comment).permit(:topic_id, :content)
+   end
+
+   def set_comment
+    @comment = Comment.find(params[:id])
+   end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+20.times do |n|
+  topic = Faker::Name.content
+  Topic.create!(content: content,
+               )
+end
+
+20.times do |n|
+  comment = Faker ::Name.comment
+  Comment.create!(comment: comment,
+                 )
+end
