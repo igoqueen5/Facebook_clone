768ca6c4eb593de5c25bd41737ea395436d79120
igoqueen5 <igoqueen5@gmail.com>
2017-08-16 14:18:27 +0000

    アカウント有効化リンクのリンク先をherokuからawsに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-config.action_mailer.default_url_options = { host: 'https://ancient-mesa-24406.herokuapp.com/' }
+config.action_mailer.default_url_options = { host: 'http://13.114.99.22/' }
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings =
 {
